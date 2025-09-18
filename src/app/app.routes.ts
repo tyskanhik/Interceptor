f915a71f754a9bf7939315ce86d8219db2404f8e
Arctic Fox <tyskanhik@mail.ru>
2025-09-18 16:07:18 +0300

    last fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Profile } from './components/profile/profile';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'login', 
+    component: Login 
+  },
+  { 
+    path: 'profile', 
+    component: Profile,
+    canActivate: [authGuard]
+  },
+  { 
+    path: '', 
+    redirectTo: '/login', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: '**', 
+    redirectTo: '/login' 
+  }
+];
